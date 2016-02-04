6cb4448f346d97a217d3348186de7554ecaaab89
mcarter78 <sjsharksftw@gmail.com>
2016-02-03 17:08:07 -0800

    adds play again button to win screen

--- app-prototype.js
+++ app-prototype.js
@@ -1,12 +1,13 @@
 var words = ["evil", "live", "veil", "vile"];
+var monsterHP = 30;
 var monstersDefeated = 0;
 var monsters = [
   {
-    name: "a Zombie",
+    name: "The Zombie",
     image: "img/zombie.jpg"
   },
   {
-    name: "a Skeleton Warrior",
+    name: "The Skeleton Warrior",
     image: "img/skeleton-warrior.jpg"
   },
   {
@@ -18,7 +19,7 @@ var monsters = [
     image: "img/dragon.jpg"
   },
   {
-    name: "a Demon",
+    name: "The Demon",
     image: "img/demon.jpg"
   },
   {
@@ -47,6 +48,9 @@ Game.prototype = {
   init: function() {
     //set event listeners
     var _this = this;
+    $("#play-again").on("click", function(e){
+      _this.resetGame();
+    });
     $("#input-form").on("submit", function(e){
       e.preventDefault();
       var $formGuess = $("#input-field");
@@ -58,12 +62,14 @@ Game.prototype = {
 
       }
     });
+
     this.newAnagram();
     this.newMonster();
   },
 
-  reset: function() {
-
+  resetGame: function() {
+    var newGame = new Game();
+    newGame.init();
   },
 
   newAnagram: function(){
@@ -76,20 +82,22 @@ Game.prototype = {
   newMonster: function(){
     // monster object lives in monsters array index 0-7
     // select a random monster (number between 0 and array.length - 1)
+    monsterHP = 30;
     var chosenMonster = monsters[this.randomNumber(monsters)];
     console.log(chosenMonster);
-      // display its name
-      $("#monster-name").html("<h1>You have been attacked by</br> <span class='red'>" + chosenMonster.name + "</span>!</h1>");
-      // display its image
-      $("#monster-image").html("<img src=" + chosenMonster.image + ">");
-      $("#monster-image").append("<h3>" + chosenMonster.name + " -- HP: " + 30 + "</h3>"); // placeholder hp
-      $("#defeated").html("<h3>Monsters Defeated: " + monstersDefeated + "</h3>");
-      // pop the monster object from the array so it will not repeat
-      monsters.splice(monsters.indexOf(chosenMonster), 1);
-      if(monsters.length === 0){
-        Game.prototype.youWin();
-      }
-      console.log(monsters);
+    // display its name
+    $("#monster-name").html("<h1>You have been attacked by</br> <span class='red'>" + chosenMonster.name + "</span>!</h1>");
+    // display its image
+    $("#monster-image").html("<img src=" + chosenMonster.image + ">");
+    $("#monster-image").append("<h3>" + chosenMonster.name + " -- HP: <span id='hp'>" + monsterHP + "</span><span id='damage' class='hidden'>  -10</span></h3>"); // placeholder hp
+    $("#defeated").html("<h3>Monsters Defeated: " + monstersDefeated + "</h3>");
+    // pop the monster object from the array so it will not repeat
+    monsters.splice(monsters.indexOf(chosenMonster), 1);
+    var _this = this;
+    if(monsters.length === 0){
+      _this.youWin();
+    }
+    console.log(monsters);
   },
 
   randomNumber: function(array){
@@ -97,6 +105,7 @@ Game.prototype = {
   },
 
   checkWords: function(guess){
+    var _this = this;
     var targetWordIndex = words.indexOf(guess);
     if (targetWordIndex != -1) { // if user guess is included in the array...
       var $newListItem = $("<li>");
@@ -104,11 +113,19 @@ Game.prototype = {
       words.splice(targetWordIndex, 1); // remove word from array
       $newListItem.html(targetWord);
       $("#guesses ul").append($newListItem);
+      _this.doDamage();
       if (words.length === 0){
         $("#input-field").submit();
       }
     }
     console.log(guess);
+  },
+
+  doDamage: function(){
+    monsterHP = monsterHP - 10;
+    $("#hp").html(monsterHP);
+    $("#damage").removeClass("hidden");
+    $("#damage").fadeOut().delay(200).addClass("hidden");       // not working -- fix later
   },
 
   defeatMonster: function(){
@@ -121,8 +138,10 @@ Game.prototype = {
   },
 
   youWin: function(){
-    $("body").empty();
-    $("body").html("<h1 class='huge red'>YOU DEFEATED ALL THE MONSTERS!</h1>");
+    $("#screen").empty();
+    $("#screen").html("<h1 class='huge red'>YOU DEFEATED ALL THE MONSTERS!</h1>");
+    $("#screen").append("<h3>Monsters Defeated: " + monstersDefeated + "</h3>");
+    $("#screen").append("<button id='play-again'>Play Again</button>");
   }
 };
 
@@ -137,9 +156,8 @@ Player.prototype = {
   }
 };
 
-var Monster = function(name, image){
+var Monster = function(name, image, hp){
   this.name = name;
-  this.hp = 30;
   this.image = image;
 };
 
