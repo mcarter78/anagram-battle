08c2c91a1c315511b8a3760108e29706cd5f7523
mcarter78 <sjsharksftw@gmail.com>
2016-04-20 18:58:38 -0700

    converts into node app with express

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res) {
+  res.sendFile('/index.html');
+});
+
+app.listen(port, function(){
+  console.log('connected to:', port);
+});
